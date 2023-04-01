bf84a6a749dfdfb6ca14e6f7ccd20ba77a0dcd6a
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-04-01 16:15:24 +0530

    updating with Eror in code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            rrror
             Console.WriteLine("Hello Jenkin, Thank You for bringing me up");
         }
     }
